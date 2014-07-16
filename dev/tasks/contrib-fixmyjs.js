f17d405cb7efcc1e15ca318ea1286992b06b720e
chr-nos <chro_nos@ukr.net>
2014-07-16 18:16:16 +0300

    Установка grunt-fixmyjs

---
+++ dev/tasks/contrib-fixmyjs.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  // fixmyjs
+  grunt.config('fixmyjs', {
+    options: {
+      config: '<%= configDir %>' + '/.jshintrc',
+      boss: true,
+      curly: true,
+      eqeqeq: false,
+      eqnull: true,
+      esnext: true,
+      expr: true,
+      forin: true,
+      immed: true,
+      laxbreak: true,
+      newcap: false,
+      noarg: true,
+      node: true,
+      nonew: true,
+      plusplus: true,
+      quotmark: 'single',
+      strict: false,
+      undef: true,
+      unused: true
+    },
+    test: {
+      files: [
+        {expand: true, src: ['js/main.js']} 
+      ]
+    }
+  });
+  grunt.loadNpmTasks('grunt-fixmyjs');
+};
