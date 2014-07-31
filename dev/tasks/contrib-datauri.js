387edec02c9460ce0372c959668a088385871430
Valery Bogdanov <radkiller@yandex.ru>
2014-07-31 13:28:12 +0300

    Fix code

--- dev/tasks/contrib-datauri.js
+++ dev/tasks/contrib-datauri.js
@@ -3,15 +3,15 @@ module.exports = function(grunt) {
   // Кодируем картинки в base64
   grunt.config('datauri', {
     datauri: {
-        options: {
-          classPrefix: 'data-',
-          checkFilesize: false
-        },
-        src: ['<%= imgDir %>' + '/base64/*.{png,jpg,gif}'],
-        dest: ['<%= sassDir %>' + '/_base64.scss']
-      }
+      options: {
+        classPrefix: 'data-',
+        checkFilesize: false
+      },
+      src: ['<%= imgDir %>' + '/base64/*.{png,jpg,gif}'],
+      dest: ['<%= sassDir %>' + '/_base64.scss']
+    }
   });
-  
+
   grunt.loadNpmTasks('grunt-datauri');
 
 };
