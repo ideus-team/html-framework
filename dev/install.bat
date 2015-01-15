de01130a9811d0cc84824c67ebf1e78bbbbc7116
Valery Bogdanov <valery.bogdanov@ideus.biz>
2015-01-15 14:04:10 +0200

    Fix update gem

--- dev/install.bat
+++ dev/install.bat
@@ -1,1 +1,1 @@
-gem update --system & gem update sass & gem update compass & npm update grunt-cli -g & npm install --save-dev
+gem update --system --source http://rubygems.org & gem update sass & gem update compass & npm update grunt-cli -g & npm install --save-dev
