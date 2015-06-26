3e9ed612277efe6c66f426bace58a15d61fe533c
delka <delka@ideus.biz>
2015-06-26 17:06:38 +0300

    Add  grunt-notify

---
+++ dev/grunt/notify.js
@@ -0,0 +1,8 @@
+module.exports = {
+  uglify: {
+    options: {
+      title   : 'Build complete',  // optional
+      message : '<%= pkg.name %> build finished successfully.' //required
+    }
+  }
+}
