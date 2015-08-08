74553bedc4f4713f3442079b47b85f04f3696d2c
Igor Zenich <delka@ideus.biz>
2015-08-09 00:23:17 +0300

    Gruntfile: refactoring last paths var (config, css & js)

--- dev/Gruntfile.js
+++ dev/Gruntfile.js
@@ -12,49 +12,47 @@ module.exports = function(grunt) {
     data: {
       // Data passed into config.  Can use with <%= test %>
 
+      // Paths for Plugins settings:
+      gruntPluginsDir : 'grunt/config',
+
       // Paths for Watch:
         // Grunt
-            gruntSettings: [
-              /* Grunt config   */ 'Gruntfile.js',
-              /* Grunt modules  */ 'package.json',
-              /* Build settings */ 'grunt/aliases.yaml',
-              /* Grunt tasks    */ 'grunt/*.js'   
-            ],
-            gruntPluginsSettings: [              
-              '<%= csslint.options.csslintrc %>',
-              '<%= jshint.options.jshintrc %>',
-            ],
-        // CSS
-            sourceCSSFiles: [
-              'src/sass/**/*.scss'
-            ],
-        // JS
-            sourceJSPlugins : [
-              'src/js/plugins/**/*.js'
-            ],              
-            sourceJSMy : [
-              'src/js/blocks/**/*.js',          
+        gruntSettings: [
+          /* Grunt config   */ 'Gruntfile.js',
+          /* Grunt modules  */ 'package.json',
+          /* Build settings */ 'grunt/aliases.yaml',
+          /* Grunt tasks    */ 'grunt/*.js'
+        ],
+        gruntPluginsSettings: [
+          '<%= csslint.options.csslintrc %>',
+          '<%= jshint.options.jshintrc %>',
+        ],
+
+        // Compile:
+          // Source:
+            // CSS
+            sourceCSSDir       : 'src/sass',
+                sourceCSSFiles : '<%= sourceCSSDir %>' + '/**/*.scss',
+            // JS
+            sourceJSPlugins    : 'src/js/plugins/**/*.js',
+            sourceJSMy         : [
+              'src/js/blocks/**/*.js',
               'src/js/main.js'
-            ],                    
-        sourceJSFiles : [
-          '<%= sourceJSPlugins %>',
-          '<%= sourceJSMy %>'
-        ],        
-      
-      // Paths for Settings:
-        gruntPluginsDir : 'grunt/config',      
-      
-      // Paths for Compile:
-        // Source
-        sourceCSSDir    : 'src/sass',              
-        sourceIMGDir    : 'src/img',              
-        // Destination
-        destCSSDir      : '../assets/css', // generated css-files names taked from scss files in %sourceCSSDir
-            destCSS     : '<%= destCSSDir %>' + '/main.css',
-        destJSDir       : '../assets/js',
-            destJS      : '<%= destJSDir %>'  + '/scripts.js',      
-        destIMGDir      : '../assets/img',    
-      
+            ],
+                sourceJSFiles : [
+                  '<%= sourceJSPlugins %>',
+                  '<%= sourceJSMy %>'
+                ],
+            // IMG
+            sourceIMGDir    : 'src/img',
+
+          // Destination:
+            destCSSDir      : '../assets/css', // generated css-files names taked from scss files in %sourceCSSDir
+                destCSS     : '<%= destCSSDir %>' + '/main.css',
+            destJSDir       : '../assets/js',
+                destJS      : '<%= destJSDir %>'  + '/scripts.js',
+            destIMGDir      : '../assets/img',
+
     }
   });
 };
