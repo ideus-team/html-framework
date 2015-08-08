ca4c9c1eac78bc19237e48bba65f253a6d19dbb7
Igor Zenich <delka@ideus.biz>
2015-08-08 20:59:20 +0300

    JSHint, Contrib: Fix bug in Gruntfile sourceJSFiles path, what didnt include all js

--- dev/Gruntfile.js
+++ dev/Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
           'src/sass/**/*.scss',
         ],
         sourceJSFiles : [
-          'src/js/**/.js',
+          'src/js/**/*.js',
         ],        
       
       // Paths for Compile:
