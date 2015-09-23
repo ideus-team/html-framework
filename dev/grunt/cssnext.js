90361623a2fa6a360394c78f63f9404fd6593e37
Igor Zenich <delka@ideus.biz>
2015-09-23 17:21:46 +0300

    Set CSSNext browsers support = as in PostCSS browser support

--- dev/grunt/cssnext.js
+++ dev/grunt/cssnext.js
@@ -1,7 +1,12 @@
 module.exports = {
 
     options: {
-      sourcemap: true
+      sourcemap: true,
+      browsers: [
+        '> 1%',
+        'Last 2 versions',
+        'IE 8'
+      ]
     },
     dist: {
       files: {
