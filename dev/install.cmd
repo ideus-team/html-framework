efc34b5f3b38d70fc64e6595c95a74a919a40938
Valery Bogdanov <valery.bogdanov@ideus.biz>
2015-10-30 13:53:05 +0200

    Remove Ruby update from unstall

--- dev/install.cmd
+++ dev/install.cmd
@@ -1,16 +1,4 @@
 @echo off
-echo Update environment...
-
-echo Update Ruby...
-call gem update --system
-
-echo Update Ruby Sass...
-call gem update sass
-
-echo Update Bourbon...
-call gem update bourbon
-call gem update neat
-call gem update bitters
 
 echo Update Grunt...
 call npm update grunt-cli -g
