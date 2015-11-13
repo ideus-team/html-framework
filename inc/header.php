befbd5263dd69939a56f76ed9ae15975ee1b5c60
Valery Bogdanov <valery.bogdanov@ideus.biz>
2015-11-13 20:00:55 +0200

    Add classes for html & body

--- inc/header.php
+++ inc/header.php
@@ -32,7 +32,7 @@
   $title = ($isHomepage) ? $siteName : $pageName.' : '.$siteName;
 ?>
 <!doctype html>
-<html class="-device_<?php echo $device; ?> no-js" lang="">
+<html class="l-html -device_<?php echo $device; ?> no-js" lang="">
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -70,7 +70,7 @@
     <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-2.1.4.min.js"><\/script>')</script>
   <!--<![endif]-->
 </head>
-<body class="-page_<?php echo $uri; ?><?php echo (!$isHomepage) ? ' -page_inner' : ''; ?>">
+<body class="l-body -page_<?php echo $uri; ?><?php echo (!$isHomepage) ? ' -page_inner' : ''; ?>">
   <!--[if lt IE 8]>
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
