b9693f0126995f08032fb8c1fc5181c2a83f861d
Valery Bogdanov <valery.bogdanov@ideus.biz>
2015-11-20 19:06:34 +0200

    Add Ruby & Sass/Compass to install.bat

--- dev/install.cmd
+++ dev/install.cmd
@@ -1,5 +1,16 @@
 @echo off
 
+echo Update environment...
+
+echo Update Ruby...
+call gem update --system
+
+echo Update Ruby Sass...
+call gem update sass
+
+echo Update Ruby Compass...
+call gem update compass
+
 echo Update Grunt...
 call npm update grunt-cli -g
 
