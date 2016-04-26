f8a2ddc9b3722e0dd760779553e442d7c59e422f
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-04-26 16:08:06 +0300

    Fix sitemap

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -1,14 +1,15 @@
 // Authors: %Author Name%
 
 $(function() {
-  if ($('html').hasClass('-device_desktop')) {}
-  initsiteMapTabs(); 
-});
-$(window).resize(function() {});
-$(window).load(function() {});
 
-function initsiteMapTabs() {
-	$('.js-sitemapTabs__title').on('click', function() {
-		$(this).toggleClass('-state_active').toggleClass('-state_inactive').removeClass('-state_default').next('.js-sitemapTabs__content').toggleClass('-state_active').removeClass('-state_default');
-	});
-}
+
+  if ($('html').hasClass('-device_desktop')) {
+
+  }
+});
+$(window).resize(function() {
+
+});
+$(window).load(function() {
+
+});
