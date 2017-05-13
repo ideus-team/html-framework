f778b988ec27f8ec097f3ca8b9d75af3b12cae73
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-05-14 01:35:27 +0300

    Fix grunt-spritesmith config

--- dev/grunt/sprite.js
+++ dev/grunt/sprite.js
@@ -1,7 +1,7 @@
 module.exports = {
   main: {
-    src: 'src/img/sprites/*.png',
-    dest: 'src/img/spritesheet.png',
-    destCss: 'src/sass/_sprites.scss',
+    src: '<%= sourceIMGDir %>' + '/sprites/*.png',
+    dest: '<%= sourceIMGDir %>' + '/spritesheet.png',
+    destCss: '<%= sourceCSSDir %>' + '/_sprites.scss',
   },
 };
