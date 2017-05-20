e95f60e5a852880e916c6548c44f4c88f6ce98e2
Valery Bogdanov <valery.bogdanov@ideus.biz>
2017-05-20 06:16:06 +0300

    add date of build js

--- dev/grunt/uglify.js
+++ dev/grunt/uglify.js
@@ -2,6 +2,7 @@ module.exports = {
   main: {
     options: {
       mangle: false,
+      banner: '/*! Build: <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %> */',
     },
     files: [{
       '<%= destJS %>': '<%= destJS %>',
