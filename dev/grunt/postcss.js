914e2eb2124e6ecbb9a0585ac3df6a409be17b77
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2019-12-27 14:41:26 +0200

    Update Grunt & packages

--- dev/grunt/postcss.js
+++ dev/grunt/postcss.js
@@ -7,15 +7,7 @@ module.exports = {
     processors: [
       require('pixrem')(), // add fallbacks for rem units
 
-      require('autoprefixer')({
-        // add vendor prefixes
-        browsers: [
-          'last 5 version',
-          'ie >= 10',
-          'iOS 4',
-          'Android 2.3',
-        ],
-      }),
+      require('autoprefixer')(), // add vendor prefixes
 
       require('cssnano')({
         convertValues: false,
