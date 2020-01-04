7668f9d6f7a516b4d6ea28f7297a5ee79baf0cd9
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2020-01-04 07:14:23 +0200

    Fix searchform accessibility error

--- inc/header.php
+++ inc/header.php
@@ -60,8 +60,11 @@
 
         <div class="l-siteSearch">
           <form class="b-siteSearch" role="search" method="post" action="#">
-            <label class="b-siteSearch__label g-visuallyhidden" for="search-text">Search:</label>
-            <input class="b-siteSearch__input" type="search" spellcheck="true" value="" name="search" id="search-text" />
+            <label class="b-siteSearch__search">
+              <span class="b-siteSearch__label g-visuallyhidden">Search:</span>
+              <input class="b-siteSearch__input" type="search" name="s" spellcheck="true" />
+            </label>
+
             <input class="b-siteSearch__button" type="submit" value="Search" />
           </form>
         </div>
