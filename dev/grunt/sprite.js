b4013e01b549856cc97cd02d515f22ffda784f87
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2020-02-15 19:33:01 +0200

    Cache problem on loading the image sprite

--- dev/grunt/sprite.js
+++ dev/grunt/sprite.js
@@ -1,10 +1,12 @@
 module.exports = {
   main: {
-    src: '<%= sourceIMGDir %>' + '/sprites/*.png',
-    dest: '<%= sourceIMGDir %>' + '/spritesheet.png',
-    destCss: '<%= sourceCSSDir %>' + '/_sprites.scss',
+    src     : '<%= sourceIMGDir %>' + '/sprites/*.png',
+    dest    : '<%= sourceIMGDir %>' + '/spritesheet.png',
+    imgPath : '<%= sourceIMGDir %>' + '/spritesheet.png?' + (new Date().getTime()),
+    destCss : '<%= sourceCSSDir %>' + '/_sprites.scss',
 
-    // retinaSrcFilter: ['<%= sourceIMGDir %>' + '/sprites/*@2x.png'],
-    // retinaDest: '<%= sourceIMGDir %>' + '/spritesheet@2x.png',
+    // retinaSrcFilter : ['<%= sourceIMGDir %>' + '/sprites/*@2x.png'],
+    // retinaDest      : '<%= sourceIMGDir %>' + '/spritesheet@2x.png',
+    // retinaImgPath   : '<%= sourceIMGDir %>' + '/spritesheet@2x.png?' + (new Date().getTime()),
   },
 };
