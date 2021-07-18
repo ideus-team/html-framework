f65a06b46fb25864d3010e24b1eeee796ccd5e94
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2021-07-18 17:13:33 +0300

    Update jQuery 3.5.1 → 3.6.0

--- inc/footer.php
+++ inc/footer.php
@@ -20,8 +20,8 @@
 
   <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
 
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.5.1.min.js"><\/script>')</script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.6.0.min.js"><\/script>')</script>
 
   <script src="assets/js/scripts.js?<?php echo filemtime( 'assets/js/scripts.js' ); ?>"></script>
 
