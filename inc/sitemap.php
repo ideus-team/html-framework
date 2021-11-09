cad2acf72fd720803022c278db27dc7fe4e42c07
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2021-11-09 14:54:31 +0200

    Fixed jQuery version

--- inc/sitemap.php
+++ inc/sitemap.php
@@ -17,8 +17,8 @@
   <link rel="stylesheet" href="assets/css/sitemap.min.css" />
   <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic|Roboto+Slab:100,300,400,700" />
 
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.1.0.min.js"><\/script>')</script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.6.0.min.js"><\/script>')</script>
 
   <script>
     $(function() {
