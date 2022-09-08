00980f344b37ab54d021cd0eb76fa189f5ab6ba7
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2022-09-08 23:18:37 +0300

    Update jQuery to 3.6.1

--- inc/footer.php
+++ inc/footer.php
@@ -18,8 +18,8 @@
     </footer>
   </div><!--wrapper-->
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.6.0.min.js"><\/script>')</script>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-3.6.1.min.js"><\/script>')</script>
 
   <script src="assets/js/scripts.js?<?php echo filemtime( 'assets/js/scripts.js' ); ?>"></script>
 
