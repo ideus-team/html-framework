0b42f9c2871d239bf19d3845a1d149de1bd03943
Valerii Bohdanov <valerii.bohdanov@ideus.biz>
2023-01-25 13:08:36 +0200

    Passive event listeners

---
+++ dev/src/js/plugins/passive.event.listeners.js
@@ -0,0 +1,26 @@
+/**
+ * Passive event listeners
+ */
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ) {
+    this.addEventListener('touchstart', handle, { passive: !ns.includes('noPreventDefault') });
+  }
+};
+
+jQuery.event.special.touchmove = {
+  setup: function( _, ns, handle ) {
+    this.addEventListener('touchmove', handle, { passive: !ns.includes('noPreventDefault') });
+  }
+};
+
+jQuery.event.special.wheel = {
+  setup: function( _, ns, handle ){
+    this.addEventListener('wheel', handle, { passive: true });
+  }
+};
+
+jQuery.event.special.mousewheel = {
+  setup: function( _, ns, handle ){
+    this.addEventListener('mousewheel', handle, { passive: true });
+  }
+};
